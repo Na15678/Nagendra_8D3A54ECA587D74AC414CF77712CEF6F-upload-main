0397e043d91bfb822a7935bf97aad9a466786328
Na15678 <144200154+Na15678@users.noreply.github.com>
2023-09-17 09:34:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-def factorial(n):
-    fact=1
-    if int(n) >= 1:
-        for i in range (1,int(n)+1):
-            fact = fact * i
-        return fact
+'''Implement a class called Player that represents a cricket player. The Player class should have a
+method called play() which prints "The player is playing cricket. Derive two classes, Batsman and
+Bowler, from the Player class. Override the play() method in each derived class to print "The batsman
+is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the
+Batsman and Bowler classes and call the play() method for each object.'''
 
-num = int(input("Enter a Number: "))
 
-print("factorial of",num,":",end="")
-print(factorial(num))
+# Define the base class Player
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+# Define the derived class Batsman
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+# Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
